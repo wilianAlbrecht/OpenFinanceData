a3516de91ab823d33139fb763151f120834c5718
wilianAlbrecht <wilian0012@hotmail.com>
2025-11-22 23:10:01 -0400

    criação do proejeto.

---
+++ src/main/java/com/financial/openFinanceData/OpenFinanceDataApplication.java
@@ -0,0 +1,13 @@
+package com.financial.openFinanceData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFinanceDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFinanceDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financial/openFinanceData/OpenFinanceDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.openFinanceData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFinanceDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
