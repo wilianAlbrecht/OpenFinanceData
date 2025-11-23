358e431f5c5518fe233b1cf0e83121874da55015
wilianAlbrecht <wilian0012@hotmail.com>
2025-11-23 07:23:31 -0400

    Adição da configuração de segurança para a aplicação, permitindo acesso sem autenticação.

---
+++ src/main/java/com/financial/openfinancedata/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.financial.openfinancedata.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(form -> form.disable())
+                .logout(logout -> logout.disable())
+                .build();
+    }
+}
